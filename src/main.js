687422be8f51c37b2d00adaae89bf602d57e0e30
Britt-H <brittain.hoover@gmail.com>
2023-03-16 13:36:10 -0400

    Updated & complete document

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 const axios = require("../utils/axios");
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:5000/constellations";
+const url = "http://localhost:5000/constellations"
 
-function getAllNames() {}
+function getAllNames() {
+  axios
+    .get(BASE_URL)
+    .then((response) => {
+      const result = response.data.map((constellation) => constellation.name)
+      console.log(result)
+    })
+}
 
-function getConstellationsByQuadrant(quadrant) {}
+function getConstellationsByQuadrant(quadrant) {
+  axios
+    .get(BASE_URL)
+    .then((response) => {
+      const result = response.data.filter((constellation) => constellation.quadrant === quadrant)
+      console.log(result)
+    })
+}
 
 module.exports = {
   getAllNames,
   getConstellationsByQuadrant,
 };
-
-
-
-//TESTTTTT
